ef1979f1e4def8e4158c9a50221c2c3f412a7098
Akansha Sakhre <asakhre2002@gmail.com>
2025-02-11 23:45:51 +0530

    added resume flow api

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const https = require("https");
+require("dotenv").config();
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const GOOGLE_SCRIPT_BASE_URL =
-  "https://script.google.com/macros/s/AKfycbzQL_J-FsO2wVp0X7Rg_LqiuPV1-955vEDhYK3F-X80dNaQVXG7jm7_UND4l81Jzygb/exec";
+const GOOGLE_SCRIPT_BASE_URL = process.env.GOOGLE_SCRIPT_API || "";
 
-const resumeFlow = async (startDate, endDate) => {
+const resumeFlow = async (startDate, endDate, contact, flowId) => {
   try {
     if (!startDate || !endDate) {
       console.log({
         error: "Missing required query parameters: startDate and endDate",
       });
     }
-
+    const token = await getAuthToken();
     const finalUrl = `${GOOGLE_SCRIPT_BASE_URL}?startDate=${encodeURIComponent(
       startDate
     )}&endDate=${encodeURIComponent(endDate)}`;
@@ -28,43 +33,45 @@ const resumeFlow = async (startDate, endDate) => {
       maxRedirects: 5,
     });
 
-    const graphqlResponse = await axios.post(
-      "https://api.prod.glific.com/api",
-      {
-        query: `
-          mutation resumeContactFlow($flowId: ID!, $contactId: ID!, $result: Json!) {
-            resumeContactFlow(flowId: $flowId, contactId: $contactId, result: $result) {
-              success
-              errors {
-                key
-                message
-              }
-            }
-          }
-        `,
-        variables: {
-          flowId: "26171",
-          contactId: "1702210",
-          result: response.data,
-        },
+    let data = JSON.stringify({
+      query: `mutation resumeContactFlow($flowId: ID!, $contactId: ID!, $result: Json!) {
+                resumeContactFlow(flowId: $flowId, contactId: $contactId, result: $result) {
+                  success
+                  errors {
+                      key
+                      message
+                  }
+                }
+              }`,
+      variables: {
+        flowId: flowId,
+        contactId: contact?.id,
+        result: JSON.stringify({
+          metrics: response?.data,
+        }),
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer YOUR_ACCESS_TOKEN`, // Replace with your actual access token
-        },
-      }
-    );
-    console.log(response.data, graphqlResponse);
+    });
+
+    let config = {
+      url: process.env.GLIFIC_URL,
+      headers: {
+        authorization: token,
+        "Content-Type": "application/json",
+      },
+    };
+
+    const data2 = await axios.post(config?.url, data, {
+      headers: config?.headers,
+      httpsAgent: agent,
+    });
   } catch (error) {
     console.log({ error: "Failed to fetch data", details: error.message });
   }
 };
 
-// Proxy route with dynamic query params
-app.get("/get-metrics", async (req, res) => {
-  const { startDate, endDate } = req.query;
-  resumeFlow(startDate, endDate);
+app.post("/get-metrics", async (req, res) => {
+  const { startDate, endDate, contact, flowId } = req.body;
+  resumeFlow(startDate, endDate, contact, flowId);
 
   res.json({
     success: true,
@@ -74,3 +81,24 @@ app.get("/get-metrics", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const getAuthToken = async () => {
+  try {
+    const glific_backend_url = process.env.GLIFIC_URL || "";
+    const response = await axios.post(
+      `${glific_backend_url}/session`,
+      {
+        user: {
+          phone: process.env.PHONE,
+          password: process.env.PASSWORD,
+        },
+      },
+      {
+        httpsAgent: agent,
+      }
+    );
+    return response.data.data.access_token;
+  } catch (error) {
+    console.log("Error in getting token", error.message);
+  }
+};
